de9790934962602abb4b039929d987aacff69441
Marcelio Leal <marcelioleal@gmail.com>
2013-05-04 19:57:14 -0300

    File example - how to add JS, CSS and HTML files

---
+++ include.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function () {
+  //How to add a JS file
+  $.when(
+    $.getScript("URLSCRIPT"),
+    $.getScript("URLSCRIPT"),
+    $.getScript("URLSCRIPT"),
+    $.Deferred( function(deferred){
+      $(defered.resolve));
+    })
+  ).done(function(){
+    //JS Code that depends
+
+    //CSS
+    jQuery('<style>#saFeedbackTitle{color:#eee;text-shadow:0 1px 0 #000}</style>').appendTo('head');                                      
+    
+    //HTML
+    jQuery('<div></div>').appendTo('body');
+  });
+});
